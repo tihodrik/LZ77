fb4311f62d7a96baaf1e15f206290cde1a67ec00
Darya Tikhomirova <tihodrik@gmail.com>
2017-05-12 01:15:54 +0300

    project finished

--- LZ77/Form1.Designer.cs
+++ LZ77/Form1.Designer.cs
@@ -28,12 +28,114 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comp = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.decomp = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 33);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(311, 64);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Повторяющийся элемент вводится по шаблону \"(a,b)\",\r\nгде a и b - целые числа, a < " +
+    "0, b > 0\r\n\r\nВНИМАНИЕ: пробелов в скобках быть не должно!";
+            // 
+            // comp
+            // 
+            this.comp.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comp.Location = new System.Drawing.Point(12, 137);
+            this.comp.Multiline = true;
+            this.comp.Name = "comp";
+            this.comp.Size = new System.Drawing.Size(311, 119);
+            this.comp.TabIndex = 0;
+            this.comp.Text = "abac(-4,3)d(-8,7)e";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Инструкция";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(13, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Сжатый текст";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 272);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(310, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Провести декомпрессию";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(13, 321);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Результат";
+            // 
+            // decomp
+            // 
+            this.decomp.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.decomp.Location = new System.Drawing.Point(12, 341);
+            this.decomp.Multiline = true;
+            this.decomp.Name = "decomp";
+            this.decomp.ReadOnly = true;
+            this.decomp.Size = new System.Drawing.Size(311, 91);
+            this.decomp.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(342, 444);
+            this.Controls.Add(this.decomp);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comp);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox comp;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox decomp;
     }
 }
 
--- LZ77/Form1.cs
+++ LZ77/Form1.cs
@@ -16,5 +16,37 @@ namespace LZ77
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string compressed = comp.Text;
+            decomp.Clear();
+            string[] fragments = compressed.Split('(', ')');
+            // После операции split во fragments получим массив строк,
+            // где под четными индексами - фрагменты текста,
+            // а под нечетными - описание повторов в формате "a,b"
+            int a, b;
+            string[] ab;
+            for (int i = 0; i < fragments.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    decomp.AppendText(fragments[i]);
+                }
+                else
+                {
+                    ab = fragments[i].Split(',');
+                    a = int.Parse(ab[0]);
+                    b = int.Parse(ab[1]);
+                    decomp.AppendText(GetRepeated(decomp.Text, a, b));
+                }
+            }
+        }
+        string GetRepeated(string range, int offset, int count) {
+            if (count > offset*(-1)) {
+                throw new ArgumentOutOfRangeException("Неверно задан повтор");
+            }
+            return  range.Substring(range.Length + offset, count);
+        }
     }
 }
